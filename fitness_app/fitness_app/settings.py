d65c9c812ca2562d05ddcc0348cf8da7ae1a6402
koqfl <koqfloqf@gmail.com>
2024-04-19 13:33:22 +0300

    docker full config

--- fitness_app/fitness_app/settings.py
+++ fitness_app/fitness_app/settings.py
@@ -148,10 +148,12 @@ LOGGING = {
     }
 }
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": "redis://redis:6379/1",
-#     }
-# }
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+    }
+}
 
