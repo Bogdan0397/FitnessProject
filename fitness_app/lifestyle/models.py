299fa269970de369d1b36e98c6ad6c534074a557
Karhopsis <yaryk23041997@gmail.com>
2024-04-29 14:21:08 +0300

    common settings

--- fitness_app/lifestyle/models.py
+++ fitness_app/lifestyle/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Lifestyle(models.Model):
-    subcategories = models.ForeignKey()
+    subcategories = models.ForeignKey(name="sub_categories")
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=100, unique=True, db_index=True)
 
 
 
 class FoodPlans(models.Model):
     meals = models.ManyToManyField()
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=100, unique=True, db_index=True)
 
 
 
 
 class Meals(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
-    content = models.CharField(max_length)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True)
+    content = models.CharField(max_length=100, blank=True)
